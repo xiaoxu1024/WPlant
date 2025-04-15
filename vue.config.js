830ba46c59e0e3028443e7cb22a3f7456d9bce9f
coderxu <coderxu@tencent.com>
2025-04-15 11:34:33 +0800

    补充配置

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,22 @@
+const path = require('path')
 const { defineConfig } = require('@vue/cli-service')
+
+function resolve(dir) {
+  return path.join(__dirname, dir)
+}
+
 module.exports = defineConfig({
-  transpileDependencies: true
+  publicPath: './', // 解决web页面打包后本地打开页面空白的问题
+  transpileDependencies: true,
+  chainWebpack: (config) => {
+    // 由于修改了渲染进程目录，修改@的alias
+    config.resolve.alias.set('@', resolve('src'))
+  },
+  css: {
+    loaderOptions: {
+      sass: {
+        // additionalData: // TODO: 这里存放全局变量
+      }
+    }
+  },
 })
