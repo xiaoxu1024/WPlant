a19057aa07e38893007e59e7bd8502bd47e2d3a0
coderxu <coderxu@tencent.com>
2025-05-06 20:03:47 +0800

    路由模式修改

--- vue.config.js
+++ vue.config.js
@@ -8,6 +8,9 @@ function resolve(dir) {
 module.exports = defineConfig({
   publicPath: './', // 解决web页面打包后本地打开页面空白的问题
   transpileDependencies: true,
+  devServer: {
+    historyApiFallback: true,
+  },
   chainWebpack: (config) => {
     // 由于修改了渲染进程目录，修改@的alias
     config.resolve.alias.set('@', resolve('src'))
